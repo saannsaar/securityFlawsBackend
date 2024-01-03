763427c5f7cf68effded49fffdf84a2427d29295
saannsaar <sannsaarinen@gmail.com>
2024-01-03 12:59:46 +0200

    uusx

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const csrf = require('@dr.pogodin/csurf')
 // app.use(cors(corsOptions))
 
 app.use(cors({
+  origin: 'https://security-issues-frontend.onrender.com',
   credentials: true,
 }))
 app.use(cookieParser());
